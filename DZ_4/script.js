a6d8c04dae34e6d023985e2c4d5507fa173d36ec
c2gdnb <neonx2007@gmail.com>
2023-09-07 11:27:30 +0200

    add homework №4

---
+++ DZ_4/script.js
@@ -0,0 +1,6 @@
+const firstNumber = +prompt("Enter first number");
+const secondNumber = +prompt("Enter second number");
+const thirdNumber = +prompt("Enter third number");
+const total = (firstNumber + secondNumber + thirdNumber) / 3;
+
+alert(total);
